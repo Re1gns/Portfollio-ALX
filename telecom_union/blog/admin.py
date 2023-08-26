51297de7e55ade3d4f78491233dd8face10ceab5
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-08-26 13:45:45 +0300

    admin.py - added few customizations to the admin panel

--- telecom_union/blog/admin.py
+++ telecom_union/blog/admin.py
@@ -3,6 +3,13 @@ from .models import Post, Comment, UserProfile
 
 # Register your models here.
 
-admin.site.register(Post)
+class CustomPost(admin.ModelAdmin):
+    list_display = ("title", "slug", "created_at", "featured")
+
+class CustomUser(admin.ModelAdmin):
+    list_display = ("user", "is_member")
+
+
+admin.site.register(Post, CustomPost)
 admin.site.register(Comment)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, CustomUser)
